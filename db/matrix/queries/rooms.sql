1a1911695d0205dcc55cd66dad51ea6def03325a
hummingbard <mail@hummingbard.com>
2024-06-05 15:52:36 +0600

    initial public room quries

--- db/matrix/queries/rooms.sql
+++ db/matrix/queries/rooms.sql
@@ -1,2 +1,26 @@
 -- name: IsRoomPublic :one
 SELECT exists(select 1 from rooms where room_id = $1 and is_public = true);
+
+-- name: GetRoomHistoryVisibility :one
+SELECT ej.json::jsonb->'content'->>'history_visibility' AS visibility 
+FROM current_state_events cse JOIN event_json ej ON ej.event_id = cse.event_id 
+WHERE cse.room_id = $1
+AND cse.type = 'm.room.history_visibility';
+
+-- name: GetRoomJoinRules :one
+SELECT ej.json::jsonb->'content'->>'join_rule' AS visibility 
+FROM current_state_events cse JOIN event_json ej ON ej.event_id = cse.event_id 
+WHERE cse.room_id = $1
+AND cse.type = 'm.room.join_rules';
+
+-- name: GetRoomJoinRules :one
+SELECT ej.json::jsonb->'content'->>'join_rule' AS visibility 
+FROM current_state_events cse JOIN event_json ej ON ej.event_id = cse.event_id 
+WHERE cse.room_id = $1
+AND cse.type = 'm.room.join_rules';
+
+-- name: GetRoomGuestAccess :one
+SELECT ej.json::jsonb->'content'->>'guest_access' AS visibility 
+FROM current_state_events cse JOIN event_json ej ON ej.event_id = cse.event_id 
+WHERE cse.room_id = $1
+AND cse.type = 'm.room.guest_access';
